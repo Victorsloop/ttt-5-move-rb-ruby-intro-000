a09668668ceaa95c7bdcb1a2be0c0ccccfa851f6
Victor Torres <torres.victor136@gmail.com>
2020-07-05 22:27:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,7 @@ def input_to_index(user_input)
   return new_user_input
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(board, index, value = "X")
+  board[index] = value
   return board
 end
